090b4277ae5a8fdf5096bbceee7beff3aa1644d5
6lueparr0t <6lueparr0t@gmail.com>
2022-05-09 00:36:28 +0900

    update programmers

---
+++ source/programmers/exam/Java12947.java
@@ -0,0 +1,18 @@
+// https://programmers.co.kr/learn/courses/30/lessons/12947
+
+public class Java12947 {
+  public boolean solution(int x) {
+    String[] list = String.valueOf(x).split("");
+    int sum = 0;
+
+    for (String s : list) {
+      sum += Integer.parseInt(s);
+    }
+
+    if (x % sum == 0) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
