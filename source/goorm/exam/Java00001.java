9600fe5cc58bb88443bcd46d8ddca2af9e2a42ef
6lueparr0t <6lueparr0t@gmail.com>
2022-05-17 16:24:36 +0900

    update code

--- source/goorm/exam/Java00001.java
+++ source/goorm/exam/Java00001.java
@@ -17,31 +17,34 @@ public class Java00001 {
 		
 		for(int i = 0; i < logs.length; i++) {
 			Map<String, Integer> msgMap = new LinkedHashMap<>();
+
 			if(i+1<logs.length && logs[i].equals(logs[i+1])) {
 				if ("".equals(repeat)) repeat = logs[i];
 				count++;
-				continue;
 			} else if(count > 1) {
 				msgMap.put(repeat, count);
 				history.put(i, msgMap);
 				count = 1;
 				repeat = "";
-				continue;
-			}
-			
-			msgMap.put(logs[i], 1);
-			history.put(i, msgMap);
+			} else {
+        msgMap.put(logs[i], 1);
+        history.put(i, msgMap);
+      }
 		}
 		
 		System.out.println(history.size());
 		for (Integer msglist : history.keySet()) {
+
 			Map<String, Integer> list = history.get(msglist);
+
 			for (String msg : list.keySet()) {
+
 				if(list.get(msg) > 1) {
 					System.out.printf("%s (%d)\n", msg, list.get(msg));
 				} else {
 					System.out.println(msg);
 				}
+        
 			}
 		}
 	}
