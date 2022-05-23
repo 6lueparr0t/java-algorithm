9c1d0caa5460d3ea9a06cd3d30025f3989596f81
6lueparr0t <6lueparr0t@gmail.com>
2022-05-23 16:41:20 +0900

    update programmers

---
+++ source/programmers/exam/Java00006.java
@@ -0,0 +1,25 @@
+// 짝 짓기 (baabaa -> baba -> 종료 1)
+import java.util.Stack;
+
+public class Java00006 {
+  public int solution(String s)
+    {
+        Stack<Character> stack = new Stack<>();
+
+        char[] list = s.toCharArray();
+        
+        for (char c : list) {
+            if(stack.size() > 0 && stack.peek() == c) {
+                stack.pop();
+            } else {
+                stack.push(c);
+            }
+        }
+
+        if(stack.size() > 0) {
+            return 0;
+        } else {
+            return 1;
+        }
+    }
+}
